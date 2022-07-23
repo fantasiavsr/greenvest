ac4fff5d5adb1dcdfe02f924b28aab2da9cc09a9
fantasiavsr <fantasiavsr@gmail.com>
2022-07-24 01:42:45 +0700

    fix Deploy

--- app/Models/Produk_green.php
+++ app/Models/Produk_green.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class produk_green extends Model
+class Produk_green extends Model
 {
     use HasFactory;
     protected $guarded = [];
