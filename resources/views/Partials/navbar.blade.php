e28c7633c708141654d0c87eff3c431755f5b189
fantasiavsr <fantasiavsr@gmail.com>
2022-07-23 15:56:50 +0700

    recreate Partials

---
+++ resources/views/Partials/navbar.blade.php
@@ -0,0 +1,40 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark pt-4 pb-3" {{-- style="background-color: #30445C" --}}>
+    <div class="container">
+
+        <a class="navbar-brand" href="/"><img class="" src="{{ asset('img/gv-light.png') }}" alt="" width="100%"
+                height="26"></a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="fw-bold nav-link {{ $title === 'Home' ? 'active' : '' }}" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="fw-bold nav-link {{ $title === 'Get Started' ? 'active' : '' }}" href="{{ route('register') }}">Get Started</a>
+                </li>
+                <li class="nav-item">
+                    <a class="fw-bold nav-link {{ $title === 'Support' ? 'active' : '' }}" href="#footer">Support</a>
+                </li>
+                {{-- <li class="nav-item">
+                    <a class="fw-bold nav-link {{ $title === 'About Us' ? 'active' : '' }}" href="#">About Us</a>
+                </li> --}}
+            </ul>
+
+            {{-- <a href="/register" class="btn rounded-pill me-4 text-dark" role="button" style="background-color: #FFFFFF"><small
+                class="fw-normal px-3">Register</small>
+            </a> --}}
+
+            <a href="{{ route('login') }}" class="btn rounded-pill me-4 text-dark" role="button" style="background-color: #FFFFFF"><small
+                    class="fw-normal px-4">Login</small>
+            </a>
+
+        </div>
+    </div>
+</nav>
