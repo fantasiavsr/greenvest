d52448e0a15cb37085230a5532309b29f6edfca9
fantasiavsr <fantasiavsr@gmail.com>
2022-07-24 04:42:44 +0700

    fix main navbar toggle

--- resources/views/Partials/navbar.blade.php
+++ resources/views/Partials/navbar.blade.php
@@ -1,8 +1,8 @@
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark pt-4 pb-3" {{-- style="background-color: #30445C" --}}>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark pt-2 pb-3">
     <div class="container">
 
-        <a class="navbar-brand" href="/"><img class="" src="{{ asset('img/gv-light.png') }}" alt="" width="100%"
+        <a class="navbar-brand mt-2" href="/"><img class="" src="{{ asset('img/gv-light.png') }}" alt="" width="100%"
                 height="26"></a>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
@@ -10,9 +10,9 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
 
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav mx-auto mt-3 mb-2 mb-lg-0">
                 <li class="nav-item">
                     <a class="fw-bold nav-link {{ $title === 'Home' ? 'active' : '' }}" href="/">Home</a>
                 </li>
@@ -22,16 +22,9 @@
                 <li class="nav-item">
                     <a class="fw-bold nav-link {{ $title === 'Support' ? 'active' : '' }}" href="#footer">Support</a>
                 </li>
-                {{-- <li class="nav-item">
-                    <a class="fw-bold nav-link {{ $title === 'About Us' ? 'active' : '' }}" href="#">About Us</a>
-                </li> --}}
             </ul>
 
-            {{-- <a href="/register" class="btn rounded-pill me-4 text-dark" role="button" style="background-color: #FFFFFF"><small
-                class="fw-normal px-3">Register</small>
-            </a> --}}
-
-            <a href="{{ route('login') }}" class="btn rounded-pill me-4 text-dark" role="button" style="background-color: #FFFFFF"><small
+            <a href="{{ route('login') }}" class="btn rounded-pill mt-3 text-dark" role="button" style="background-color: #FFFFFF"><small
                     class="fw-normal px-4">Login</small>
             </a>
 
